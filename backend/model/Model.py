f0d89d1ca8acc0196c0a147e0ec28a933e24ea63
Andrea Bioddo <andrea.bioddo@gmail.com>
2025-05-01 15:11:53 +0200

    changed model

--- backend/model/Model.py
+++ backend/model/Model.py
@@ -13,8 +13,11 @@ class MitigationModel(BaseModel):
 class BenefitModel(BaseModel):
     description: str = Field(description="Description of the benefit")
 
+class RiskMitigation(BaseModel):
+    risk: RiskModel = Field(description="Risk associated with the use case")
+    mitigations: List[MitigationModel] = Field(description="Mitigation strategies associated with the risk")
+    
 class RiskBenefitsMitigation(BaseModel):
     usecase_title: str = Field(description="Title of the use case")
-    risks: List[RiskModel] = Field(description="Risk and mitigation associated with the use case")
-    mitigation: List[MitigationModel] = Field(description="Mitigation strategy associated with the use case")
+    risks: List[RiskMitigation] = Field(description="Risk and mitigation associated with the use case")
     benefits: List[BenefitModel] = Field(description="Benefit associated with the use case")
